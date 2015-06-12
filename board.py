31cd30a9bb5d36b4b4fb97cac26c78b3d1514ce1
moog500 <zoomaa500@gmail.com>
2015-06-12 09:32:51 -0400

    added output smoke weed kush dank dank dank

--- board.py
+++ board.py
@@ -31,15 +31,26 @@ def printBoard(bord):
             html+= lett + ' '
         html+= '<br>'
     return html
+generatedmeme = generate()
 
- 
+def getstring(bord):
+    html = '?'
+    for i in range(4):
+        for j in range(4):
+            letter = bord[i][j]
+            html += str(i + 1) + str(j + 1) + '=' + letter
+    return html
+
+
 htmlStr = "<html><head><title> Random Boggle Board</title></head></html>\n"
 htmlStr += "<body>"
 htmlStr += '<h3>Your Board:</h3><br><br>'
-htmlStr += printBoard(generate())
+htmlStr += printBoard(generatedmeme)
 htmlStr += '<br>Want to <a href="board.py">get another board?</a><br>'
+htmlStr += 'Or <a href="solve.py'+getstring(generatedmeme)+'">solve this board?</a>'
 htmlStr += 'Or <a href="boggle.html">go back?</a><br>'
 htmlStr += "</body></html>"
+
 
 
 print htmlStr
